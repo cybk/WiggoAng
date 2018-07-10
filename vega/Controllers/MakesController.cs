cf534e1d2e5046fe0665aa0c5fe3bc9c01243e55
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-07-09 19:24:58 -0500

    Added the first method on api

--- vega/Controllers/MakesController.cs
+++ vega/Controllers/MakesController.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Controllers
 {
     public class MakesController : Controller
     {
+        private readonly VegaDBContext _context;
+
+        public MakesController(VegaDBContext context)
+        {
+            this._context = context;
+        }
         
+        
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes(){
+            return await _context.Makes.Include( m => m.Models).ToListAsync();
+
+
+        }
+
     }
 }
