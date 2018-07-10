55cf848553005a303538764bcad740c93c87b350
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-07-09 19:19:34 -0500

    Added the seed for data in sql

---
+++ vega/Migrations/20180709234928_SeedDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModeLA', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModeLB', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModeLC', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModeLA', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModeLB', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModeLC', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModeLA', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModeLB', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModeLC', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Makes");
+            migrationBuilder.Sql("Delete from Models");
+        }
+    }
+}
