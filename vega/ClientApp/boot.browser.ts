701f6b26cf946fe436bc1e04d0ebddf94e0495d3
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-07-10 20:02:28 -0500

    Added routing for new component
    Added too the initial form elements on page

--- vega/ClientApp/boot.browser.ts
+++ vega/ClientApp/boot.browser.ts
@@ -3,7 +3,7 @@ import 'zone.js';
 import 'bootstrap';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.browser.module';
+import { AppModule } from './other modules/app.browser.module';
 
 if (module.hot) {
     module.hot.accept();
